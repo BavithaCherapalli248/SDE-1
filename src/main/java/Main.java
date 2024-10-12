d02ba5b8a8c390964365369e4f76002561f04354
BavithaCherapalli248 <BC001014951@techmahindra.com>
2024-10-12 05:32:18 +0000

    hash file modified

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,16 @@ class Main{
     Scanner sc = new Scanner(System.in);
     String s=sc.nextLine();
     int q=sc.nextInt();
-
-    int hash[26]={0};
+    int hash[]=new int[26];
+    Arrays.fill(hash,0);  
     for(int i=0;i<s.length();i++){
       hash[s.charAt(i)-'a'] += 1;
     }
-
     
-    while(q--){
-      Char c = sc.next().charAt(0);
-
-    System.out.println(hash[c -'a']);
+    while(q>0){
+      char c = sc.next().charAt(0);
+      System.out.println(hash[c -'a']);
+      q--;
     }
   }
 }
