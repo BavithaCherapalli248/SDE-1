4fe0ccbf3341a3fe25c33bfb3a3050a837142855
BavithaCherapalli248 <bavithacherapalli@gmail.com>
2024-10-24 16:38:25 +0530

    conversion.java

---
+++ src/main/java/conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float curr=sc.nextInt();
+        float usd=curr/(float)84.04;
+        System.out.printf("%.2f",usd);
+        
+    }
+}
